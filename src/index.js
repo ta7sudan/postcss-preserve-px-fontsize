542aaaeed07d082bf950f4369def45a970918a9c
ta7sudan <ta7sudan@gmail.com>
2018-11-11 21:55:54 +0800

    bug fixed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 const postcss = require('postcss');
 
 function getOriginPx(fontSize, viewportWidth) {
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
+'use strict';
 const test = require('ava');
 
 /* global todo */
 
 
-const sleep = time => new Promise(rs => setTimeout(rs, time));
-const isObj = o => Object.prototype.toString.call(o) === '[object Object]';
+// const sleep = time => new Promise(rs => setTimeout(rs, time));
+// const isObj = o => Object.prototype.toString.call(o) === '[object Object]';
 
 test('todo', async t => {
 	t.pass();
